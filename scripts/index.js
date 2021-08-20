486eb77f0f58a3046d7ee568b99770b75ac2ed05
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-08-21 03:30:38 +0600

    updated price table

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let basePrice = 1299;
 let promoCode = "stevekaku";
-
+//html elements
 let extraMemoryPrice = document.getElementById("extra-memory-price");
 let extraStoragePrice = document.getElementById("extra-storage-price");
 let deliveryCharge = document.getElementById("delivery-charge");
@@ -8,7 +8,7 @@ let totalPrice = document.getElementById("total-price");
 let total = document.getElementById("total");
 let promoCodeInputBox = document.getElementById("promo-code");
 
-//event listener
+//event listeners
 document.getElementById("buttons").addEventListener("click", (event) => {
     applyExtraCost(event.target.id.toString());
 });
@@ -21,48 +21,42 @@ function applyExtraCost(id) {
     switch (id) {
         case "8gb-memory":
             extraMemoryPrice.innerText = 0;
-            updatePrices();
             break;
         case "16gb-memory":
             extraMemoryPrice.innerText = 180;
-            updatePrices();
             break;
         case "256gb-storage":
             extraStoragePrice.innerText = 0;
-            updatePrices();
             break;
         case "512gb-storage":
             extraStoragePrice.innerText = 100;
-            updatePrices();
             break;
         case "1tb-storage":
             extraStoragePrice.innerText = 180;
-            updatePrices();
             break;
         case "aug25":
             deliveryCharge.innerText = 0;
-            updatePrices();
             break;
         case "aug21":
             deliveryCharge.innerText = 20;
-            updatePrices();
             break;
         default:
     }
+    updatePrices();
 }
 
 function updatePrices() {
     totalPrice.innerText = total.innerText = basePrice + parseInt(extraMemoryPrice.innerText) + parseInt(extraStoragePrice.innerText) + parseInt(deliveryCharge.innerText);
 }
 
-function applyPromoCode(code) {
-    if (code === promoCode) {
-        total.innerText = parseInt(totalPrice.innerText) - parseInt(totalPrice.innerText) * 0.20;
-    }
-}
-
 function extractPromoCode() {
     let code = promoCodeInputBox.value.toString();
     promoCodeInputBox.value = "";
     return code;
+}
+
+function applyPromoCode(code) {
+    if (code === promoCode) {
+        total.innerText = parseInt(totalPrice.innerText) - parseInt(totalPrice.innerText) * 0.20;
+    }
 }
